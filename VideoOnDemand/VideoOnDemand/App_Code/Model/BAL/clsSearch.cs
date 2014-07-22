591ad36463e3444472cd548316d3237d492348f5
avgsnaidu <vgsnaidu11@outlook.com>
2014-07-22 16:58:15 +0530

    search Saved..

--- VideoOnDemand/VideoOnDemand/App_Code/Model/BAL/clsSearch.cs
+++ VideoOnDemand/VideoOnDemand/App_Code/Model/BAL/clsSearch.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,25 @@ namespace VideoOnDemand.Model.BAL
 {
     public class clsSearch
     {
+        public DataSet fnSearch(string searchKeyword, string searchOn, bool IsArabic)
+        {
+
+            DataSet ds;
+
+            SqlParameter[] p = new SqlParameter[3];
+            p[0] = new SqlParameter("@SearchKey", SqlDbType.VarChar);
+            p[0].Value = searchKeyword;
+            p[1] = new SqlParameter("@SearchOn", SqlDbType.VarChar);
+            p[1].Value = searchOn;
+            p[2] = new SqlParameter("@IsArabic", SqlDbType.VarChar);
+            p[2].Value = IsArabic;
+             
+            ds = SqlHelper.ExecuteDataset(ClsConnectionString.getConnectionString(), CommandType.StoredProcedure, "usp_SearchText", p);
+            return ds;
+
+
+
+
+        }
     }
 }
